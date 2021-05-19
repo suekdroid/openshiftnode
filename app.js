acf1c33beeb9f668e337d54e2a348136aef16f70
suekdroid <nikolajskousen@gmail.com>
2021-05-19 09:25:28 +0200

    ip 0.0.0.0

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const ip = '0.0.0.0'
+const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, ip, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
